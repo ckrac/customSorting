17a865cbb01f59d86896207953e0136a08e2f3f5
ckrac <kennethracadio@hotmail.com>
2018-01-18 19:23:49 +0000

    can sort alpha but still need to solve same names

---
+++ prac.js
@@ -0,0 +1,30 @@
+// the array to be sorted
+var list = ['Delta', 'alpha', 'CHARLIE', 'bravo'];
+
+// temporary array holds objects with position and sort-value
+var mapped = list.map(function(el, i) {
+  return { index: i, value: el.toLowerCase() };
+})
+
+// console.log(mapped);
+
+// sorting the mapped array containing the reduced values
+mapped.sort(function(a, b) {
+  if (a.value > b.value) {
+    return 1;
+  }
+  if (a.value < b.value) {
+    return -1;
+  }
+  return 0;
+});
+
+// console.log(mapped);
+
+
+// container for the resulting order
+var result = mapped.map(function(el){
+  return list[el.index];
+});
+
+console.log(result);
